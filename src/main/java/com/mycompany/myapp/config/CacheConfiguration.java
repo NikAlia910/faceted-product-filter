230e2aabb0068dd882f101946c54dff0bb31c408
WeiKang756 <weikang756@gmail.com>
2025-05-30 17:16:08 +0800

    First Commit

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -41,6 +41,8 @@ public class CacheConfiguration {
             createCache(cm, com.mycompany.myapp.repository.UserRepository.USERS_BY_LOGIN_CACHE);
             createCache(cm, com.mycompany.myapp.repository.UserRepository.USERS_BY_EMAIL_CACHE);
             createCache(cm, com.mycompany.myapp.domain.Authority.class.getName());
+            createCache(cm, com.mycompany.myapp.domain.Category.class.getName());
+            createCache(cm, com.mycompany.myapp.domain.Product.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
